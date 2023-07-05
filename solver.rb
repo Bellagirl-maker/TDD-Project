c146b46a34629c62466d2352c34f0829e74642b7
Oumaima Nejjari <oumanj1998@gmail.com>
2023-07-05 19:10:33 +0100

    Updating solver file

--- solver.rb
+++ solver.rb
@@ -9,4 +9,11 @@ class Solver
     word.reverse
   end
 
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
